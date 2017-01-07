6a87b5a8c25ba2849bc9fb8fce13c8798eb62db5
Adrien Siami <adrien.siami@gmail.com>
2017-01-07 16:10:14 +0100

    Add photos model

---
+++ app/models/photo.rb
@@ -0,0 +1,10 @@
+class Photo < ApplicationRecord
+
+  validates :title, presence: true
+
+  belongs_to :user
+
+  mount_uploader :photo, PhotoUploader
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126150813) do
+ActiveRecord::Schema.define(version: 20170107103100) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.string   "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
